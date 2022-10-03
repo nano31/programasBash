817760b1aa62ea6f3a6bd4e3b9bada02e4d19c98
Franco Garcia <francogarcia1331@gmail.com>
2022-10-03 20:39:56 -0300

    ej18.sh

---
+++ ej18.sh
@@ -0,0 +1,13 @@
+# ! /bin/bash
+
+if [ $# -ne 1 ] then
+    echo "No se pasaron los parametros necesarios"
+    exit 1
+else
+    while [ $1 != "$(who | cut -d" " -f1 | grep $1)" ]; do
+        echo "Buscando..."
+        sleep 10
+    done
+    echo "Usuario $1 logeado en el sistema"
+fi
+exit 0
